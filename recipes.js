33b3b319d39c7f10b426be7ec96e28669cc063a3
Josh Bailey <127659714+BaileyJoJo@users.noreply.github.com>
2023-05-12 10:03:18 +0100

    1.2 done

--- recipes.js
+++ recipes.js
@@ -5,18 +5,23 @@ const fileName = "recipes.json";
 
 // GET ALL RECIPES
 export async function getRecipes() {
-    const allFood = await fs.readFile(fileName)
+    const allFood = await fs.readFile(fileName);
     const allRecipes =JSON.parse(allFood)
     return(allRecipes)
 }
-
-getRecipes()
-
-/* we need to await, parse and return 
-
-
 // GET A RECIPE BY ID
-export async function getRecipeByID(id) {}
+export async function getRecipeByID(id) {
+    const allFood = await fs.readFile(fileName);
+    const allRecipes =JSON.parse(allFood)
+for (let i = 0; i < allRecipes.length; i++) {
+if (allRecipes[i].id === id) {
+     console.log (allRecipes[i]) 
+     return(allRecipes[i])
+
+}}} 
+
+getRecipeByID("3")
+
 
 // CREATE A RECIPE
 export async function createRecipe(newRecipe) {}
