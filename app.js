4918f8cbccaece2bb3bc552e1ece0fb9f8e5dc1e
Josh Bailey <127659714+BaileyJoJo@users.noreply.github.com>
2023-05-12 12:07:21 +0100

    updated func added

--- app.js
+++ app.js
@@ -25,67 +25,22 @@ app.get("/api/recipes/:id", async function (req, res) {
   res.status(200).send(recipe);
 });
 
+app.post("/api/recipes", async function (req, res) {
+  const newRecipe = req.body;
+  const recipe = await createRecipe(newRecipe); 
+  res.status(201).send(recipe);
+})
+
+app.patch("/api/recipes/:id", async function (req, res) {
+  const id = req.params.id;
+  const changedRecipe = req.body;
+  const recipe = await updateRecipeByID(id, changedRecipe); 
+  res.status(200).send(recipe);
+})
+
+
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-/*
-
-import express from "express";
-const app = express();
-const PORT = 3000;
-
-import { getQuotes, addQuote, getRandomQuote, editQuote, deleteQuote } from "./quote.js";
-
-app.use(express.json());
-
-app.get("/", function (req, res) {
-  res.send("Welcome to cwissy.rest");
-});
-
-app.get("/api/quotes", async function (req, res) {
-  if (req.query.type === "random") {
-    return res.send(await getRandomQuote());
-  }
-  res.send(await getQuotes());
-});
-
-app.post("/api/quotes", async function (req, res) {
-  const { quoteText } = req.body;
-  const quote = await addQuote(quoteText);
-  res.status(201).send(quote);
-});
-
-app.patch("/api/quotes/:id", async function (req, res) {
-  const quoteId = req.params.id;
-  
-  console.log(quoteId);
-
-  const updatedQuoteText = req.body.quoteText;
-
-  // Call the appropriate helper function to update the quote in your data
-  const updatedQuote = await editQuote(quoteId, updatedQuoteText);
-
-  res.send(updatedQuote);
-
-});
-
-app.delete("/api/quotes/:id", async function (req, res) {
-  const quoteId = req.params.id;
-
-  // Call the appropriate helper function to delete the quote from your data
-  const deletedQuote = await deleteQuote(quoteId);
-
-  res.send(deletedQuote);
-});
-  
-app.listen(PORT, function () {
-  console.log(`Server is now listening on http://localhost:${PORT}`);
-});
-
-
-/* alt app.post("/api/quotes", async function (req, res) {
-  const { quoteText } = req.body;
-  const quote = await addQuote(quoteText);
-  res.status(201).send(quote);
-}); */ 
